28433c60981dffe0cff887c332c9a0920542cca5
hobeom <hobeom2049@gmail.com>
2025-03-08 02:25:12 +0900

    optimization

--- code/06-caching/01-starting-project/app/messages/page.js
+++ code/06-caching/01-starting-project/app/messages/page.js
@@ -1,9 +1,10 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
 
+export const revalidate = 5;
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
+  const response = await fetch("http://localhost:8080/messages", {
     headers: {
-      'X-ID': 'page',
+      "X-ID": "page",
     },
   });
   const messages = await response.json();
--- code/07-optimizations/01-starting-project/app/page.js
+++ code/07-optimizations/01-starting-project/app/page.js
@@ -1,7 +1,8 @@
-import { Suspense } from 'react';
-
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import { Suspense } from "react";
+import logo from "@/assets/logo.png";
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+import Image from "next/image";
 
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
@@ -14,9 +15,9 @@ export default async function Home() {
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
       <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
